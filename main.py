1877379beda2cd75c5c6d5a43f90f0787dc7481c
Medrzyk <kuba.uksorzel@gmail.com>
2026-05-07 20:11:52 +0200

    Etap 3

--- main.py
+++ main.py
@@ -1,52 +1,80 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv("Data/shopping_trends.csv")
+from transformers import pipeline
+
+df = pd.read_csv("Data/amazon.csv")
 
 #Eksploracja danych
-# print(df.head()) #Wyswietla 5 pierwszych wierszy z tabeli
-# print(df.columns) #Wyswietla wszystkie nazwy kolumn
-# print(df.shape) #Wielkosc tabeli danych
-# print(df.isnull().sum()) #Sprawdzamy czy znajduja sie puste wiersze, jezeli tak to ile
-# print(df.describe()) #Opis zebranych danych
-# print(df["Shipping Type"].unique())
-# print(df.groupby("Season")["Season"].count())
-# print(df["Item Purchased"].unique())
-# print(df.groupby("Season")["Item Purchased"].value_counts().head(5))
-# print(df.groupby("Discount Applied")["Purchase Amount (USD)"].mean())
-# print(df["Subscription Status"].unique())
-# print(df.groupby("Subscription Status")["Purchase Amount (USD)"].mean()) 
-# print(df["Category"].unique())
-# print(df.groupby("Category")["Purchase Amount (USD)"].mean())
 
-dane = df.groupby("Shipping Type")["Purchase Amount (USD)"].mean()
-print(dane)
+#print(df)
+#print(df.isnull().sum())
+#print(df.columns)
+#print(df["discount_percentage"].head(15)) 
+#print(df.loc[df["rating_count"].isnull(), "rating_count"])
+# df = df.dropna(subset=["rating_count"]) # Usuniecie pustych wierszy
+#print(df.isnull().sum())
+# print(df.describe())
+# print(df["rating"].unique())
+# print(df["category"].unique())
+# print(df.groupby("rating")["rating"].count())
+# print(df["product_name"].unique())
+# print(df["category"].value_counts().head(5))
+# print(df["discount_percentage"].unique())
+df = df[df["rating"] != "|"]
+# print(df.loc[df["rating"] == "|", ["product_name", "rating", "discount_percentage"]])
+df["rating"] = df["rating"].astype(float)
+df["discount_percentage"] = df["discount_percentage"].str.replace("%", "").astype(float)
 
-#Wykres 1 (Czy rodzaj dostawy wpływa na kwotę zakupu?)
-dane.plot(kind="bar")
-plt.title("Srednia kwota zakupu wzgledem rodzaju dostawy")
-plt.xlabel("Rodzaj dostawy")
-plt.ylabel("Srednia kwota (USD)")
+# 1 pytanie badawcze (Jaki jest wplyw rabatow na zadowolenie klientow)
+plt.figure(figsize=(10, 6))
+plt.scatter(df["discount_percentage"], df["rating"], alpha=0.5, color="teal")
+plt.title("Wplyw wysokosci rabatu na ocene produktu")
+plt.xlabel("Znizka (%)")
+plt.ylabel("Ocena (1-5)")
+plt.grid(True, linestyle="--", alpha=0.6)
 plt.tight_layout()
 plt.savefig("outputs/Wykres1.png")
 plt.show()
 
-# Wykres 2 Jak sezony wplywaja na popularność produktów?
-dane2 = df.groupby("Season")["Item Purchased"].value_counts().groupby(level=0).head(5)
-dane2.unstack(level=0).plot(kind="bar", figsize=(15,6),color=["orange", "green", "red", "blue"])
-plt.title("Top 5 produktow w kazdym sezonie")
-plt.xlabel("Produkt")
-plt.ylabel("Liczba zakupow")
+#2 pytanie badawcze (Jakie kategorie generuja nawiecej opini?)
+df["rating_count"] = df["rating_count"].str.replace(",", "").astype(float)
+df["main_category"] = df["category"].str.split("|").str[0]
+popularnosc = df.groupby("main_category")["rating_count"].sum().sort_values(ascending=False).head(5)
+
+plt.figure(figsize=(10, 6))
+popularnosc.plot(kind="bar", color="skyblue", edgecolor="black")
+plt.title("5 najpopularniejszych kategorii na Amazonie wedlug ilosci ocen")
+plt.xlabel("Glowna kategoria")
+plt.ylabel("Laczna liczba opinii (w milionach)")
 plt.tight_layout()
 plt.savefig("outputs/Wykres2.png")
 plt.show()
 
-#Wykres 3 Która kategoria produktów generuje największe przychody?
-dane3 = (df.groupby("Category")["Purchase Amount (USD)"].mean())
-dane3.plot(kind="bar")
-plt.title("Która kategoria produktów generuje największe przychody?")
-plt.xlabel("Kategoria")
-plt.ylim(55,62)
-plt.ylabel("Srednia kwota zakupu")
+#3 pytanie badawcze (Czy ocena wplywa na dlugosc opini?)
+df["review_length"] = df["review_content"].astype(str).str.len()
+df["rating_rounded"] = df["rating"].round()
+dlugosc_opinii = df.groupby("rating_rounded")["review_length"].mean()
+plt.figure(figsize=(10, 6))
+dlugosc_opinii.plot(kind="bar", color="mediumpurple", edgecolor="black")
+plt.title("Srednia dlugosc recenzji a ocena produktu")
+plt.xlabel("Ocena produktu (zaokraglona)")
+plt.ylabel("Srednia liczba znakow w recenzji")
+plt.xticks(rotation=0)
 plt.tight_layout()
 plt.savefig("outputs/Wykres3.png")
 plt.show()
+
+
+#Etap 3 model transformers Distilbert 
+
+df_sample = df.sample(n=50, random_state=42).copy()
+sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+def get_sentiment(text):
+    text = str(text)[:512]
+    result = sentiment_pipeline(text)[0]
+    return result["label"]
+df_sample["ai_sentiment"] = df_sample["review_content"].apply(get_sentiment)
+df_sample["rating_category"] = df_sample["rating"].apply(lambda x: "POSITIVE" if x >= 4 else ("NEGATIVE" if x <= 3 else "NEUTRAL"))
+zgodnosc = (df_sample["ai_sentiment"] == df_sample["rating_category"]).mean() * 100
+print(df_sample[["rating", "rating_category", "ai_sentiment", "review_content"]].head(10))
+print(f"Zgodnosc modelu AI z ocenami klientow: {zgodnosc}%")
